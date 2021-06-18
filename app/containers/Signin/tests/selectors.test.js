9cd2d348166fd0b307e00ae84d1e2e0becd9055c
felixwong <felix011018@gmail.com>
2021-06-18 05:41:58 +0000

    create signin

---
+++ app/containers/Signin/tests/selectors.test.js
@@ -0,0 +1,7 @@
+// import { selectSigninDomain } from '../selectors';
+
+describe('selectSigninDomain', () => {
+  it('Expect to have unit tests specified', () => {
+    expect(true).toEqual(false);
+  });
+});
