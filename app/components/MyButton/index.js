7f96cf7c1448ff0fd94c375e5ab5bf3be8433051
felix0110 <33945353+felix0110@users.noreply.github.com>
2021-06-29 13:58:27 +0800

    Add files via upload

---
+++ app/components/MyButton/index.js
@@ -0,0 +1,37 @@
+/**
+ *
+ * Button.js
+ *
+ * A common button, if you pass it a prop "route" it'll render a link to a react-router route
+ * otherwise it'll render a link with an onclick
+ */
+
+import React, { Children } from 'react';
+import PropTypes from 'prop-types';
+
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+export default withStyles((theme) => ({
+  root: {
+    borderRadius: 0,
+    fontWeight: theme.typography.fontWeightMedium,
+    fontFamily: theme.typography.fontFamilySecondary,
+    padding: theme.spacing(2, 4),
+    fontSize: theme.typography.pxToRem(14),
+    boxShadow: 'none',
+    '&:active, &:focus': {
+      boxShadow: 'none',
+    },
+  },
+  sizeSmall: {
+    padding: theme.spacing(1, 3),
+    fontSize: theme.typography.pxToRem(13),
+  },
+  sizeLarge: {
+    padding: theme.spacing(2, 5),
+    fontSize: theme.typography.pxToRem(16),
+  },
+}))(Button);
+
+
